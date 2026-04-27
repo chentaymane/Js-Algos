1f0cd5854d859b73ff1067e54b67c140b5acf17b
achent <chentaymane234@gmail.com>
2026-04-27 14:39:25 +0100

    min-max

---
+++ min-max.js
@@ -0,0 +1,15 @@
+function max(v1 , v2){
+    if (v1>= v2){
+        return v1
+    }else {
+        return v2
+    }
+}
+
+function min(v1 , v2){
+    if (v1<= v2){
+        return v1
+    }else {
+        return v2
+    }
+}
