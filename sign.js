9241c7ab5d33bb3a973bc49d2e775a770819275a
achent <chentaymane234@gmail.com>
2026-04-27 14:43:08 +0100

    sign

---
+++ sign.js
@@ -0,0 +1,12 @@
+function sign (v){
+    if (v === 0 ) {return 0}
+    if (v > 0) {
+        return 1
+    }else {
+        return -1
+    }
+}
+
+function sameSign(v1,v2){
+    if (sign(v1) && sign(v2)){return true }else{return false}
+}
