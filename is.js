b60936f49c972282443065fad83da3bd9aa2735b
achent <chentaymane234@gmail.com>
2026-04-27 15:03:22 +0100

    is

--- is.js
+++ is.js
@@ -7,7 +7,7 @@ const is = {}
   is.undef=  function(v) { return typeof v === 'undefined' }
   is.def=    function(v) { return typeof v !== 'undefined' }
   is.arr=    function(v) { return Array.isArray(v) }
-  is.obj=    function(v) { return typeof v === 'object' && !Array.isArray(v) }
+  is.obj=    function(v) { return typeof v === 'object' && !Array.isArray(v) && v!=null }
   is.fun=    function(v) { return typeof v === 'function' }
   is.truthy= function(v) { return !!v }
   is.falsy=  function(v) { return !v }
