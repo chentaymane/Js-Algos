22689407f39e23f27a8440cfff17889f43e284b4
achent <chentaymane234@gmail.com>
2026-04-27 11:55:57 +0100

    more-or-less

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more (v ){
+    return v++
+}
+
+function less (v) {
+    return v--
+}
+
+function add (v1 , v2){
+    return v1++ , v2++
+}
+
+function sub(v1 , v2){
+    return v1-v2
+}
