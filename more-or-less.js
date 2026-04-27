6dfade441aa6b9523b93b14eca9e7fb79bd2432e
achent <chentaymane234@gmail.com>
2026-04-27 11:59:58 +0100

    more-or-less

--- more-or-less.js
+++ more-or-less.js
@@ -1,13 +1,13 @@
 function more (v ){
-    return v++
+    return ++v
 }
 
 function less (v) {
-    return v--
+    return --v
 }
 
 function add (v1 , v2){
-    return v1++ , v2++
+    return v1+v2
 }
 
 function sub(v1 , v2){
