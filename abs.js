80a4b62aaba85c69016b26c96e2307e23965478e
achent <chentaymane234@gmail.com>
2026-04-27 14:28:33 +0100

    abs

--- abs.js
+++ abs.js
@@ -1,15 +1,16 @@
 function isPositive(v) {
   if (v < 0) {
     return false
-  } else if (v === 0) {
-    return false
   } else {
     return true
   }
 }
 
 function abs(v) {
-    if (!isPositive(v)){
+    if (v==0 ){
+       return 0
+    
+    }else if(!isPositive(v)){
         return -v
 
     }else {
