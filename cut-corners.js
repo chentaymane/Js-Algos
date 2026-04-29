dcb3f27bb7410701a21203bc13cfd4b520fe7b7f
achent <chentaymane234@gmail.com>
2026-04-29 15:13:40 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -81,9 +81,3 @@ function round(n) {
     }
 }
 
-
-const nums = [3.7, -3.7, 3.1, -3.1]
-console.log(nums.map(round))
-console.log(nums.map(floor))
-console.log(nums.map(trunc))
-console.log(nums.map(ceil))
