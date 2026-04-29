92722b606ccdf1161c419f20b84f25562f4c63ac
achent <chentaymane234@gmail.com>
2026-04-29 15:28:43 +0100

    reverser

--- reverser.js
+++ reverser.js
@@ -1,9 +1,23 @@
 function reverse(arr) {
     var result =[]
+    var string = false
+    var result2 =""
+    if (typeof arr === "string"){
+        string = true
+    }
     for (let i=arr.length-1 ; i>=0 ; i--){
-        result.push(arr[i])
+        if (string){
+            result2+=arr[i]
+        }else{
+
+            result.push(arr[i])
+        }
+    }
+    if (string){
+           return result2
     }
     return result
 }
 
-console.log(reverse([4,5,8,6,1,85]))
+
+console.log(reverse("ffssfef")  )
