d57304a0250f7218443ca7f86bb702f4ad903ea0
achent <chentaymane234@gmail.com>
2026-04-29 15:04:08 +0100

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,103 +1,80 @@
-function round(n) {
-    if (n === Infinity){
-        return Infinity
-    }
-      if (n === -Infinity){
-        return -Infinity
-    }
-    var sign = 1
-    if (n <0){
-        n=-n
-    sign = -1
-    }
-    var n2 = parseInt(n,10)
-
-    num = n-n2
-    if (num === 0) {
-return sign*n
-    }else if (num < 0.5){
-        return sign*n2
-    }else {
-        return sign*(n2+1)
-    }
-
-    
-}
-function floor(n) {
-    var sign = 1
- 
-    if (n === Infinity){
-        return Infinity
-    }
-      if (n === -Infinity){
-        return -Infinity
-    }
-    if (n <0){
-        n=-n
-    sign = -1
-    }
-    var n2 = parseInt(n,10)
-
-    num = n-n2
-    if (num === 0) {
-return sign*n
-
-}    else if (sign>0) {
-     return sign*n2
-    }else {
-        return sign*(n2+1)
-    }
-
-    
-}
-function ceil(n) {
-
-    if (n === Infinity){
-        return Infinity
-    }
-      if (n === -Infinity){
-        return -Infinity
-    }
-    var sign = 1
-    if (n <0){
-        n=-n
-    sign = -1
-    }
-
-    var n2 = parseInt(n,10)
-    num = n-n2
-
-    if (num === 0) {
-        return sign*n
-    }else if (sign < 0){
-        return sign*n2
-    }else {
-        return sign*(n2+1)
-    }
-
-    
-}
 function trunc(n) {
-    if (Object.is(n, -0)) {
-        return -0
-    }
-    if (n === Infinity) {
-        return Infinity
-    }
-    if (n === -Infinity) {
-        return -Infinity
-    }
+    if (Object.is(n, -0)) return -0
+    if (n === Infinity) return Infinity
+    if (n === -Infinity) return -Infinity
     var sign = 1
     if (n < 0) {
         n = -n
         sign = -1
     }
-    n = parseInt(n, 10)
-    return sign * n
+   
+    var n2 = 0
+    while (n2 + 1 <= n) {
+        n2++
+    }
+    
+    return sign * n2
 }
 
+function floor(n) {
+    if (Object.is(n, -0)) return -0
+    if (n === Infinity) return Infinity
+    if (n === -Infinity) return -Infinity
+    var sign = 1
+    if (n < 0) {
+        n = -n
+        sign = -1
+    }
+    var n2 = trunc(n)       
+    var num = n - n2
+    if (num === 0) {
+        return sign * n
+    } else if (sign > 0) {
+        return sign * n2
+    } else {
+        return sign * (n2 + 1)
+    }
+}
 
+function ceil(n) {
+    if (Object.is(n, -0)) return -0
+    if (n === Infinity) return Infinity
+    if (n === -Infinity) return -Infinity
+    var sign = 1
+    if (n < 0) {
+        n = -n
+        sign = -1
+    }
+    var n2 = trunc(n)       
+    var num = n - n2
+    if (num === 0) {
+        return sign * n
+    } else if (sign < 0) {
+        return sign * n2
+    } else {
+        return sign * (n2 + 1)
+    }
+}
 
+function round(n) {
+    if (Object.is(n, -0)) return -0
+    if (n === Infinity) return Infinity
+    if (n === -Infinity) return -Infinity
+    var sign = 1
+    if (n < 0) {
+        n = -n
+        sign = -1
+    }
+    var n2 = trunc(n)      
+    var num = n - n2
+    if (num === 0) {
+        return sign * n
+    } else if (num < 0.5) {
+        return sign * n2
+    } else {
+        return sign * (n2 + 1)
+    }
+}
 
 
 /*[ 4, -4, 3, -3 ]
