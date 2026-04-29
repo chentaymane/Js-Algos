3ab9f0c8f48dc900b5112ca8f4f7f7b483991f17
achent <chentaymane234@gmail.com>
2026-04-29 17:57:17 +0100

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -1,10 +1,15 @@
 function pyramid(str , n) {
     var result = ""
+    var lkhwa = ""
+    for (let i=0 ;i<str.length ; i++){
+        lkhwa+=" "
+    }
     for (let i = 1 ; i <= n ; i++){
-        result += " ".repeat(n-i)+str.repeat(i)+str.repeat(i-1)+"\n"
+        result += lkhwa.repeat(n-i)+str.repeat(i)+str.repeat(i-1)+"\n"
     }
     return result.slice(0,-1)
 }
 
 
-console.log(pyramid("*",5))
+console.log(pyramid('{}', 5))
+console.log(pyramid('*', 5))
