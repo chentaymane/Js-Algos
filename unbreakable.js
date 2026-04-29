259862d5fde847090cf7fc7d8013c73af339b66f
achent <chentaymane234@gmail.com>
2026-04-29 16:23:26 +0100

    unbreakable

---
+++ unbreakable.js
@@ -0,0 +1,58 @@
+function split(str, splt) {
+    
+    var array = []
+    var tempStr = ""
+
+
+    var from =0
+
+
+    for(let i =0 ; i< str.length ; i++){
+      
+        var beginIndex = str.indexOf(splt,from)
+     
+           console.log(beginIndex)
+           console.log(i)
+        if (i != beginIndex){
+            tempStr += str[i]
+        }else{
+             if (tempStr.length>0) {
+
+         
+                 array.push(tempStr)
+                 tempStr = ""
+                 
+                 
+                }
+                 i+=splt.length-1
+                 
+                 from = beginIndex+1
+        }
+    }
+     if (tempStr.length>0) {
+
+         array.push(tempStr)
+       
+        }
+        
+    return array
+    
+}
+
+
+function join(arry, elmnt)  {
+var result = ""
+    for (let i=0 ; i<arry.length ; i++) {
+        if (i<arry.length -1) {
+
+            result += arry[i] + elmnt
+        }else{
+
+            result += arry[i] 
+        }
+
+    }
+    return result
+}
+
+console.log(join(["Fire", "Air", "Water"],","))
