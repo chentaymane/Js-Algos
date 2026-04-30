91011d834af3be0c869ca03d40d4438a22576f6f
achent <chentaymane234@gmail.com>
2026-04-30 11:48:59 +0100

    flat

---
+++ flat.js
@@ -0,0 +1,31 @@
+function flat(arr , n) {
+    var result = []
+    
+    for (let i = 0 ; i<arr.length ; i++ ) {
+      if ( Array.isArray(arr[i])&&n>0 ){
+          var sub = flat(arr[i] , n-1)
+            result.push(...sub)
+        }else {
+            result.push(arr[i])
+        }
+     
+    }
+  /* var i = 0
+   var j = 0
+   while (n!=0 ){
+       if ( Array.isArray(arr[i]) ){
+          n--
+           
+           result.push(...arr[i])
+          
+           var sub = flat(arr[i],n)
+         
+        }else {     
+            result.push(arr[i])
+        }
+        i++
+   }*/
+    return result
+}
+
+console.log(flat([1, [2, [3, 4]], 5], 1))
