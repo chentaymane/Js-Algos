caa96a50ea3075ad895d4eddf30529cdf7b32643
achent <chentaymane234@gmail.com>
2026-04-30 15:23:46 +0100

    find-expression

--- find-expression.js
+++ find-expression.js
@@ -1,3 +1,6 @@
+const add4 = '+4'
+const mul2 = '*2'
+
 function findExpression(to, from = 1, path = "1") {
 
     if (from === to) {
@@ -8,13 +11,13 @@ function findExpression(to, from = 1, path = "1") {
         return undefined
     }
 
-    let tryMul = findExpression(to, from * 2, path + mul2)
+    let tryMul = findExpression(to, from * 2, path + " "+mul2)
 
     if (tryMul !== undefined) {
         return tryMul
     }
 
-    let tryAdd = findExpression(to, from + 4, path + add4)
+    let tryAdd = findExpression(to, from + 4, path + " "+ add4)
 
     return tryAdd
 }
