8cbcc6d0e54afcd2bf8a716330a88720c72f9d8d
achent <chentaymane234@gmail.com>
2026-05-01 11:59:05 +0100

    letter-space-number

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,9 @@
 function letterSpaceNumber(str) {
     const reg = /[a-zA-Z] \d(?![\da-zA-Z])/g ;
+    if (str.match(reg) === null ){
+        return []
+    }
     return str.match(reg)
 }
 
-console.log(letterSpaceNumber('example 1, example 20'))
+console.log(letterSpaceNumber('I like 7up.'))
