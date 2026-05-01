7d92268739b5b7d1fdb1406e0910228d8508e9ed
achent <chentaymane234@gmail.com>
2026-05-01 14:19:24 +0100

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,6 @@
 
     function sameAmount(str,rg1,rg2) {
+     
         var count1 = str.match(rg1)
         if (count1 === null) {
             count1 = []
@@ -8,7 +9,7 @@
          if (count2 === null) {
             count2 = []
         }
-        
+        console.log(count1 , count2)
             return count1.length === count2.length
     }
 
@@ -23,4 +24,7 @@
 
 
 
-console.log(sameAmount("qqqqqqq abc qqqqqqq", /qqqqqqq/g, /abc/g))
+console.log(sameAmount("hello", /l/, /h/))
+
+console.log(sameAmount("q qqqqqqq q qqqqqqq", /q /, /qqqqqqq/))
+console.log(sameAmount("aaaaa", /aa/g, /aaa/g))
