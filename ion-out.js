814dd6c5895e6a90912bfd63219dd720fb1ef49b
achent <chentaymane234@gmail.com>
2026-05-01 14:46:47 +0100

    ionOut

---
+++ ion-out.js
@@ -0,0 +1,13 @@
+function ionOut(str) {
+    const reg = /tion$/
+    var arr = str.split(" ")
+    var result = []
+    for (let c of arr ){
+        if (reg.test(c)) {
+            result.push(c.slice(0,c.length-"ion".length))
+        }
+    }
+    return result
+}
+
+console.log(ionOut("attention action station"))
