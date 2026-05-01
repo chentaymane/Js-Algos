9b70a5c987d6c59b97fc6fab2f3449ded2cfda40
achent <chentaymane234@gmail.com>
2026-05-01 12:40:54 +0100

    same-amount

--- same-amount.js
+++ same-amount.js
@@ -1,7 +1,13 @@
 
     function sameAmount(str,rg1,rg2) {
         var count1 = str.match(rg1)
+        if (count1 === null) {
+            count1 = []
+        }
         var count2 = str.match(rg2)
+         if (count2 === null) {
+            count2 = []
+        }
         
             return count1.length === count2.length
     }
