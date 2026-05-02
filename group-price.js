5448218dd9d698ae9309138a271fd4594d3da624
achent <chentaymane234@gmail.com>
2026-05-02 13:11:50 +0100

    group-price

--- group-price.js
+++ group-price.js
@@ -1,5 +1,5 @@
 function groupPrice(str) {
-    const reg = /[A-Z]{3}[0-9]+\.[0-9]{2}/g
+    const reg = /[A-Z\$]{1,3}[0-9]+\.[0-9]{2}/g
     var result = []
 
     let matches = str.match(reg)
@@ -17,4 +17,4 @@ function groupPrice(str) {
 
     return result
 }
-console.log(groupPrice("USD12.31 USD5.99"))
+console.log(groupPrice("USD12.31 USD5.99 The price of the cereals is $4.00."))
