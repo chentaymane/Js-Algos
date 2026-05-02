e32309428d13f0de57e696f44c3e57ddc45e87a9
achent <chentaymane234@gmail.com>
2026-05-02 14:22:01 +0100

    valid-ip

--- valid-ip.js
+++ valid-ip.js
@@ -1,7 +1,12 @@
 function findIP(str) {
   const reg = /\b(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})(:\d{1,5})?\b/g
 
-  return str.match(reg) || []
-}
+  let matches = str.match(reg) || []
 
-console.log(findIP("hello 192.255.1.1 test 10.0.0.1:8080 bad 256.1.1.1 255.255.255.0 0.0.0.0:6876"))
+  
+  return matches.filter(ip => {
+    if (!ip.includes(':')) return true
+    let port = parseInt(ip.split(':')[1])
+    return port <= 65535
+  })
+}
