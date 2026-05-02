4180d00c06d256602ad256e11ae48fe97da9ff56
achent <chentaymane234@gmail.com>
2026-05-02 13:10:21 +0100

    group-price

---
+++ group-price.js
@@ -0,0 +1,20 @@
+function groupPrice(str) {
+    const reg = /[A-Z]{3}[0-9]+\.[0-9]{2}/g
+    var result = []
+
+    let matches = str.match(reg)
+    if (!matches) return []
+
+    for (let r of matches) {
+        var temp = []
+
+        temp.push(r)
+        temp.push(r.match(/[0-9]+(?=\.)/)[0])
+        temp.push(r.match(/(?<=\.)[0-9]{2}/)[0])
+
+        result.push(temp)
+    }
+
+    return result
+}
+console.log(groupPrice("USD12.31 USD5.99"))
