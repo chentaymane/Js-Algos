83a4fa86341c62b57d03cd66e98baf2169abbdd2
achent <chentaymane234@gmail.com>
2026-05-05 22:20:30 +0100

    day-of-the-year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,18 +1,13 @@
 function dayOfTheYear(date) {
     let d = new Date(date) 
-   console.log(d.getFullYear());
-   
-    
-  
+
     let first = new Date(0);
-    first.setFullYear(d.getFullYear(), 0, 1);
+   first.setUTCFullYear(d.getUTCFullYear(), 0, 1)
    
-   let  diff = new Date(d-first)
-    if (diff.getTime()<0){
-        -diff.getTime()
-    }
+   let  diff = d-first
+ 
    
-    return Math.floor(diff.getTime()/1000/60/60/24)
+    return Math.floor((diff/1000/60/60/24))+1
 }
 
 console.log(dayOfTheYear('0001-02-15'));
