01a4c7e14fee92fc5a1811d57e329c7a29c7a727
achent <chentaymane234@gmail.com>
2026-05-06 22:25:13 +0100

    flow

---
+++ flow.js
@@ -0,0 +1,17 @@
+function flow(arr) {
+    return function (...args) {
+        first = arr[0]
+        arr =   arr.slice(1) 
+        return arr.reduce((p,c)=>  c(p)   ,first(...args))
+    }
+  
+} 
+    
+
+
+const square = (nbr) => nbr * nbr
+const add2Numbers = (nbr1, nbr2) => nbr1 + nbr2
+
+const flowedFunctions = flow([add2Numbers, square])
+console.log(flowedFunctions(2, 3));
+ // -> 25
