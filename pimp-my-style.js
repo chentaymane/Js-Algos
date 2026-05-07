7aba0abb4f6b3a1cb9ce4bea31cb95297a42ced5
achent <chentaymane234@gmail.com>
2026-05-07 20:58:22 +0100

    pimp-my-style

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,21 +1,34 @@
-import { styles} from "./pimp-my-style.data.js";
+import { styles } from "./pimp-my-style.data.js";
 
-let unpimp =false
-let i = 0 
+let unpimp = false;
+let i = 0;
+
 export function pimp() {
-    let but = document.querySelector('.button')
-        if (!unpimp){
-             but.classList.remove('unpimp')
-            but.classList.add(styles[i])
-            i++
-            if (i === styles.length) unpimp = true
-        }else{
-            but.classList.add('unpimp')
-            i--
-            
-            but.classList.remove(styles[i])
-            if (i === 0) unpimp =false 
+    let but = document.querySelector('.button');
+
+    if (unpimp) {
+        but.classList.add('unpimp');
+    } else {
+        but.classList.remove('unpimp');
+    }
+
+    if (!unpimp) {
+        // ADD MODE
+        but.classList.add(styles[i]);
+        i++;
+
+        if (i === styles.length) {
+            unpimp = true;
         }
-    
-    
+
+    } else {
+        // REMOVE MODE
+        i--;
+
+        but.classList.remove(styles[i]);
+
+        if (i === 0) {
+            unpimp = false;
+        }
+    }
 }
