86765f801b2f4b755fda1b37e1d1807a7a81d9b3
achent <chentaymane234@gmail.com>
2026-05-07 14:34:30 +0100

    get-them-all

---
+++ get-them-all.js
@@ -0,0 +1,25 @@
+
+
+export function getArchitects() {
+    let a = document.querySelectorAll('a')
+    let span  = document.querySelectorAll('span')
+    return [a,span]
+}
+
+export function getClassical() {
+    let classical = document.querySelectorAll('a.classical')
+    let nonClassical = document.querySelectorAll('a:not(.classical)')
+    return [classical,nonClassical]
+}
+
+export function getActive() {
+    let active = document.querySelectorAll('a.classical.active')
+    let nonActive = document.querySelectorAll('a.classical:not(.active)')
+    return [active,nonActive]
+}
+
+function getBonannoPisano() {
+     let BonannoPisano = document.querySelectorAll('#BonannoPisano')
+    let nonActive = document.querySelectorAll('a.classical.active')
+    return [BonannoPisano,nonActive]
+}
