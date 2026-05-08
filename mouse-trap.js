b25df9541871fad28fd7229d849ca67d1ba34271
achent <chentaymane234@gmail.com>
2026-05-08 20:23:42 +0100

    mouse-trap

--- mouse-trap.js
+++ mouse-trap.js
@@ -3,7 +3,6 @@ let trapped = false;
 
 export function createCircle() {
     document.addEventListener('click', (e) => {
-
         div = document.createElement('div');
         div.classList.add('circle');
 
@@ -17,26 +16,28 @@ export function createCircle() {
 }
 
 export function moveCircle() {
-
     document.addEventListener('mousemove', (e) => {
-
         if (!div) return;
 
         const box = document.querySelector('.box').getBoundingClientRect();
 
         const nextLeft = e.clientX - 25;
         const nextTop = e.clientY - 25;
-        const nextRight = e.clientX + 25;
-        const nextBottom = e.clientY + 25;
+
+        const circleRect = {
+            left: nextLeft,
+            right: nextLeft + 50,
+            top: nextTop,
+            bottom: nextTop + 50
+        };
 
         const inside =
-            nextLeft > box.left + 1 &&
-            nextRight < box.right - 1 &&
-            nextTop > box.top + 1 &&
-            nextBottom < box.bottom - 1;
+            circleRect.left > box.left + 1 &&
+            circleRect.right < box.right - 1 &&
+            circleRect.top > box.top + 1 &&
+            circleRect.bottom < box.bottom - 1;
 
         if (!trapped) {
-
             div.style.left = nextLeft + 'px';
             div.style.top = nextTop + 'px';
 
@@ -44,9 +45,7 @@ export function moveCircle() {
                 trapped = true;
                 div.style.background = 'var(--purple)';
             }
-
         } else {
-
             if (inside) {
                 div.style.left = nextLeft + 'px';
                 div.style.top = nextTop + 'px';
