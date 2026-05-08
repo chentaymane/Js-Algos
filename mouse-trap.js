e7cc4409b245ca324ce8e129985410c9452876a9
achent <chentaymane234@gmail.com>
2026-05-08 20:02:09 +0100

    mouse-trap

---
+++ mouse-trap.js
@@ -0,0 +1,59 @@
+
+let div = null
+let trapped = false 
+
+export function createCircle() {
+
+    document.addEventListener('click', (e) => {
+
+         div = document.createElement('div');
+
+        div.style.left = e.clientX-25 + "px";
+        div.style.top = e.clientY-25 + "px";
+        div.style.backgroundColor='white'
+          div.dataset.block = "false";
+        div.classList.add('circle')
+
+        document.body.append(div);
+        trapped = false
+    });
+    
+
+}
+export function moveCircle() {
+
+    document.addEventListener('mousemove', (e) => {
+            if (!div) return;
+
+            
+           
+            let boxRect = document.querySelector('.box').getBoundingClientRect()
+            let inside =
+            e.clientX-25 >= boxRect.left &&
+            e.clientX +25 <= boxRect.right &&
+            e.clientY -25 >= boxRect.top &&
+            e.clientY+25 <= boxRect.bottom
+            if (!trapped) {
+                div.style.left = e.clientX - 25 + 'px';
+                div.style.top = e.clientY - 25 + "px";
+                if (inside) {
+                    trapped = true 
+                    div.style.backgroundColor = 'var(--purple)'
+                }
+            }else {
+                 if (inside) {                  
+                    div.style.left = e.clientX - 25 + 'px';
+                    div.style.top = e.clientY - 25 + "px";
+                }
+            }
+                
+    });
+}
+
+
+export function setBox() {
+    let box = document.createElement('div')
+    box.classList.add('box')
+    document.body.append(box)
+    
+}
