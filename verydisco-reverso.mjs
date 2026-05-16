0f34f8ca93a158ba4f88c227224a835e192a0f34
achent <chentaymane234@gmail.com>
2026-05-16 14:25:40 +0100

    verydisco-reverso

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -1,18 +1,25 @@
-import { readFile }from 'node:fs/promises';
-import { resolve }from 'node:path';
+import { readFile } from 'node:fs/promises'
+
 async function logFile() {
   try {
-    const filePath = resolve(`./${process.argv[2]}`);
-    const contents = await readFile(filePath, { encoding: 'utf8' });
-    let result = [] 
+    const contents = (await readFile(process.argv[2], 'utf8')).trim()
+
+    let result = []
+
     for (let arg of contents.split(' ')) {
-   let splt = Math.ceil(arg.length / 2)
-   let word = arg.slice(splt,arg.length)+arg.slice(0,splt) 
-    result.push(word) 
-}
-    console.log(result.join(' '));
+      let splt = Math.floor(arg.length / 2)
+
+      let word =
+        arg.slice(splt) +
+        arg.slice(0, splt)
+
+      result.push(word)
+    }
+
+    console.log(result.join(' '))
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message)
   }
 }
-logFile();
+
+logFile()
