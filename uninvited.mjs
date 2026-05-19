80705e8bbe952c5df5e9350e50918e8b39d372d2
achent <chentaymane234@gmail.com>
2026-05-19 17:26:00 +0100

    uninvited

---
+++ uninvited.mjs
@@ -0,0 +1,41 @@
+import * as http from 'node:http';
+import * as fs from 'node:fs';
+
+const port = 5000;
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        const guest = req.url.slice(1, req.url.length);
+        const filename = './guests/' + guest + '.json';7
+        fs.writeFile(filename, body, (err) => {
+          if (err) {
+            // If there was an error writing the file
+            res.writeHead(500, { 'Content-Type': 'application/json' });
+            body = { error: 'server failed' };
+            res.end(JSON.stringify(body));
+          } else {
+            // If the file was successfully written
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(body);
+          }
+        });
+      } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        body = { error: 'server failed' };
+        res.end(JSON.stringify(body));
+      }
+    });
+  }
+});
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
